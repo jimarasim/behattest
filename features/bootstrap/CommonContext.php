77f1b7cdf1f7ad499e48a8c9a30e483e8a1b3dcb
JAMES K ARASIM <jaemzware@hotmail.com>
2018-06-13 22:37:33 -0700

    added screenshot on test failure

--- features/bootstrap/CommonContext.php
+++ features/bootstrap/CommonContext.php
@@ -1,6 +1,7 @@
 <?php
 
 use Behat\Behat\Context\Context;
+use Behat\Behat\Hook\Scope\AfterStepScope;
 use Utilities\WebDriverFactory;
 
 /**
@@ -17,6 +18,18 @@ class CommonContext implements Context{
     public static function anOpenChromeBrowser()
     {
         CommonContext::$driver = WebDriverFactory::getDriver('chrome');       
+    }
+    
+    /** @AfterStep */
+    public static function afterStep(AfterStepScope $scope)
+    {
+        //TAKE SCREENSHOT IF STEP FAILED
+        if(!$scope->getTestResult()->isPassed()) {
+            $screenshotpath = getcwd().'/screenshots/'.date('YmdHis').'.png';
+            print('FAIL:'.$scope->getStep()->getText().' SCREENSHOT:'.$screenshotpath);
+            
+            CommonContext::$driver->takeScreenshot($screenshotpath);
+        }
     }
     
     /**
