1d20972a9b4f592c3cfb4e8d21f379d98f5ae5ad
JAMES K ARASIM <jaemzware@hotmail.com>
2018-06-24 12:59:42 -0700

    debugging laptop element shot difference

--- features/bootstrap/Utilities/Screenshot.php
+++ features/bootstrap/Utilities/Screenshot.php
@@ -47,6 +47,7 @@ class Screenshot {
         $element_src_x = $element->getLocation()->getX()-$pageXOffset;
         $element_src_y = $element->getLocation()->getY()-$pageYOffset;
         print(" X:".$element_src_x." Y:".$element_src_y." WIDTH:".$element_width." HEIGHT:".$element_height."\n");
+        print(" window.innerWidth:".$driver->executeScript("return window.innerWidth;")." window.innerHeight:".$driver->executeScript("return window.innerHeight;")."\n");
         
         // crop element screenshot from whole page screenshot, and save it to destination 
         $src = imagecreatefrompng($screenshot);
